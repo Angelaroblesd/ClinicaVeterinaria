25245b98f9b7889a179ed84de7709e7c68989fc4
Angela Robles <ang.robles@duocuc.cl>
2026-05-02 23:55:20 -0400

    model mascota

--- src/main/java/com/example/clinica_veterinaria/clinica_veterinaria/model/Mascota.java
+++ src/main/java/com/example/clinica_veterinaria/clinica_veterinaria/model/Mascota.java
@@ -1,5 +1,41 @@
 package com.example.clinica_veterinaria.clinica_veterinaria.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Mascota {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @NotNull(message = "La edad es obligatoria")
+    @Min(value = 0, message = "La edad no puede ser negativa")
+    private Integer edad;
+
+    @NotBlank(message = "La especie es obligatoria")
+    private String especie;
+
+    private String raza;
+
+    @NotBlank(message = "El nombre del dueño es obligatorio")
+    private String nombreDueno;
 }
