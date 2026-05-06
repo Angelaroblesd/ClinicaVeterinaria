81021bdcca4504b755ca99e9120ff855be982b09
franverac111 <fran.verac@duocuc.cl>
2026-05-05 20:33:46 -0400

    peuBA

---
+++ ClinicaVeterinaria-main/src/main/java/com/example/clinica_veterinaria/clinica_veterinaria/repository/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package com.example.clinica_veterinaria.clinica_veterinaria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.clinica_veterinaria.clinica_veterinaria.model.Consulta;
+
+public interface ConsultaRepository extends  JpaRepository<Consulta, Integer>{
+
+}
